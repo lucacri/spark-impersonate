e849a818cbdfa9e0f6c2ea0bd7986df6b0dfb8ba
Luca Critelli <lucacri@gmail.com>
2019-10-17 16:26:22 -0400

    Update mix

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,4 +1,4 @@
-Nova.booting((Vue, router) => {
+Nova.booting((Vue, router, store) => {
     Vue.component('index-spark-impersonate', require('./components/IndexField'));
     Vue.component('detail-spark-impersonate', require('./components/DetailField'));
 })
